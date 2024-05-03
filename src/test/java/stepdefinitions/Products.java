98f1fcc046e97c565aadd3c4ace0b8326a745183
АДМИН <kbddimitrov@gmail.com>
2024-05-03 03:57:53 +0300

    response code as 200

--- src/test/java/stepdefinitions/Products.java
+++ src/test/java/stepdefinitions/Products.java
@@ -40,7 +40,7 @@ public class Products {
     public void user_should_receive_the_response_code_as(Integer int1) {
 
         ResponseCode = response.statusCode();
-        assertEquals(ResponseCode, 200);
+        assertEquals(ResponseCode, 20);
 
     }
 
